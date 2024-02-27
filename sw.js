df10d7025b2c7e377560dfcdfc3facf1d2552a66
tslilyai <tslilyai@gmail.com>
2024-02-27 13:16:01 -0500

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231108.105954';
+const cacheName = 'chirpy-20240227.131255';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
