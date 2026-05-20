3972225ad548fefec8e1ab86f741394a28639dd4
tslilyai <tslilyai@gmail.com>
2026-05-19 21:43:09 -0700

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260520.003852';
+const cacheName = 'chirpy-20260520.004251';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
